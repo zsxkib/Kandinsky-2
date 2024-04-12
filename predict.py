a4fce49dafc34f75902932f648611921f6d345d8
Sakib <sakib@replicate.com>
2024-04-12 13:00:47 +0000

    model optimised (pget + local files + float16)

--- predict.py
+++ predict.py
@@ -2,6 +2,8 @@
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
 import os
+import time
+import subprocess
 from typing import List
 from diffusers import KandinskyV22Pipeline, KandinskyV22PriorPipeline
 import torch
@@ -10,13 +12,46 @@ from diffusers.models import UNet2DConditionModel
 
 from cog import BasePredictor, Input, Path
 
-
 MODEL_CACHE = "weights_cache"
+PRIOR_URL = "https://weights.replicate.delivery/default/kandinsky-2-2/models--kandinsky-community--kandinsky-2-2-prior.tar"
+DECODER_URL = "https://weights.replicate.delivery/default/kandinsky-2-2/models--kandinsky-community--kandinsky-2-2-decoder.tar"
+
+os.environ["HF_DATASETS_OFFLINE"] = "1"
+os.environ["TRANSFORMERS_CACHE"] = MODEL_CACHE
+
+
+def download_weights(url, dest):
+    start = time.time()
+    print("[!] Initiating download from URL: ", url)
+    print("[~] Destination path: ", dest)
+    try:
+        subprocess.check_call(["pget", "-xvf", url, dest], close_fds=False)
+    except subprocess.CalledProcessError as e:
+        print(
+            f"[ERROR] Failed to download weights. Command '{' '.join(e.cmd)}' returned non-zero exit status {e.returncode}."
+        )
+        raise
+    print("[+] Download completed in: ", time.time() - start, "seconds")
 
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
+
+        # Download the prior model if not available locally
+        prior_path = os.path.join(
+            MODEL_CACHE, "models--kandinsky-community--kandinsky-2-2-prior"
+        )
+        if not os.path.exists(prior_path):
+            download_weights(PRIOR_URL, prior_path)
+
+        # Download the decoder model if not available locally
+        decoder_path = os.path.join(
+            MODEL_CACHE, "models--kandinsky-community--kandinsky-2-2-decoder"
+        )
+        if not os.path.exists(decoder_path):
+            download_weights(DECODER_URL, decoder_path)
+
         device = torch.device("cuda:0")
 
         self.negative_prior_prompt = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature"
@@ -24,8 +59,10 @@ class Predictor(BasePredictor):
         image_encoder = (
             CLIPVisionModelWithProjection.from_pretrained(
                 "kandinsky-community/kandinsky-2-2-prior",
+                torch_dtype=torch.float16,
                 subfolder="image_encoder",
                 cache_dir=MODEL_CACHE,
+                local_files_only=True,
             )
             .half()
             .to(device)
@@ -33,8 +70,10 @@ class Predictor(BasePredictor):
         unet = (
             UNet2DConditionModel.from_pretrained(
                 "kandinsky-community/kandinsky-2-2-decoder",
+                torch_dtype=torch.float16,
                 subfolder="unet",
                 cache_dir=MODEL_CACHE,
+                local_files_only=True,
             )
             .half()
             .to(device)
@@ -44,12 +83,14 @@ class Predictor(BasePredictor):
             image_encoder=image_encoder,
             torch_dtype=torch.float16,
             cache_dir=MODEL_CACHE,
+            local_files_only=True,
         ).to(device)
         self.decoder = KandinskyV22Pipeline.from_pretrained(
             "kandinsky-community/kandinsky-2-2-decoder",
             unet=unet,
             torch_dtype=torch.float16,
             cache_dir=MODEL_CACHE,
+            local_files_only=True,
         ).to(device)
 
     def predict(
@@ -115,6 +156,11 @@ class Predictor(BasePredictor):
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
         ),
+        output_format: str = Input(
+            description="Output image format",
+            choices=["webp", "jpeg", "png"],
+            default="webp",
+        ),
     ) -> List[Path]:
         """Run a single prediction on the model"""
         if seed is None:
@@ -148,7 +194,7 @@ class Predictor(BasePredictor):
 
         output_paths = []
         for i, sample in enumerate(output.images):
-            output_path = f"/tmp/out-{i}.png"
+            output_path = f"/tmp/out-{i}.{output_format}"
             sample.save(output_path)
             output_paths.append(Path(output_path))
 
